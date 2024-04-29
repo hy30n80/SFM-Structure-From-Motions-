e0ed21583e3a32ea5f25d936c80fdcf589bc090e
hy30n80 <jjun8030@gist.ac.kr>
2024-04-30 02:39:36 +0900

    at home

--- sfm_module.py
+++ sfm_module.py
@@ -45,6 +45,16 @@ class SFM():
 
 
 
+    def randomsample(self, p1, p2):
+        p1p2 = np.concatenate((p1,p2), axis=1)
+        random_idx = np.random.randint(p1p2.shape[0], size=5)
+        p1p2_ = p1p2[random_idx,:]
+
+        p1s = p1p2_[:,:2]
+        p2s = p1p2_[:,2:]
+        return p1s, p2s
+
+
     def Estimation_E(self, matches_good, img1_kp, img2_kp):
         query_idx = [match.queryIdx for match in matches_good] #1370
         train_idx = [match.trainIdx for match in matches_good] #1370
@@ -52,17 +62,25 @@ class SFM():
         p1 = np.float32([img1_kp[ind].pt for ind in query_idx]) # (1370,2)
         p2 = np.float32([img2_kp[ind].pt for ind in train_idx]) # (1370,2)
         
+
         focal = 1698.8796645
         fx, fy = 971.7497705, 647.7488275
 
-        E, mask = cv2.findEssentialMat(p1, p2, method=cv2.RANSAC, focal=focal, pp=(fx, fy), maxIters = 500, threshold=1) # E:(3,3) Matrix
-        pdb.set_trace()
-        #return E, p1, p2
-        pass
+        """ RANSAC 직접 구현
+        for i in range(iterations):
+            p1s, p2s = self.randomsample(p1, p2)
+            pdb.set_trace()
+        """
+
+        E, mask = cv2.findEssentialMat(p1, p2, method=cv2.RANSAC, focal=focal, pp=(fx, fy), maxIters = 500, threshold=1)
+        p1 = p1[mask.ravel()==1] #left image inlier
+        p2 = p2[mask.ravel()==1] #right image inlier
+
+        return E, p1, p2
 
 
     def EM_Decomposition(self, E, p1, p2):
-
+            
         #return CM
         pass
 
