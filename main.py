869f5b2f0699b5e149c2bd6f65d1809b6fb5edd4
unknown <jjun8030@gist.ac.kr>
2024-05-02 19:37:23 +0900

    all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sfm_module import SFM
 import pdb
+import cv2
 
 
 if __name__=="__main__":
@@ -10,13 +11,12 @@ if __name__=="__main__":
     sfm = SFM()
     img1, img2 = sfm.load_image(img_path, img1_name, img2_name)
     matches_good, img1_kp, img2_kp = sfm.SIFT(img1, img2)
-
-    E, p1_inlier, p2_inlier = sfm.Estimation_E(matches_good, img1_kp, img2_kp)
-    # CameraMatrix = sfm.EM_Decomposition(E, p1_inlier, p2_inlier)
-    # Rt0, Rt1 = sfm.initialize_CM(CameraMatrix)
-    # p1, p2 = sfm.rescale_point(p1_inlier, p2_inlier, len(p1_inlier))
-    # point3d = sfm.make_3dpoint(p1, p2)
-    # sfm.visualize_3d(point3d)
+    E, inlier_cnt, p1_inlier, p2_inlier = sfm.Estimation_E(matches_good, img1_kp, img2_kp)
+    #pdb.set_trace()
+    EM0, EM1, p1_normalized, p2_normalized = sfm.EM_Decomposition(E, p1_inlier, p2_inlier)
+    #pdb.set_trace()
+    point3d  = sfm.LinearTriangulation(EM0, EM1, p1_normalized, p2_normalized)
+    sfm.visualize_3d(point3d)
 
 
     
--- sfm_module.py
+++ sfm_module.py
@@ -3,11 +3,18 @@ import cv2
 import sys
 from matplotlib import pyplot as plt
 import pdb
+import matlab.engine
+
 
 class SFM():
     def __init__(self):
-        pass
-
+        self.eng = matlab.engine.start_matlab()
+        self.eng.addpath(r'C:\app\SfM', nargout=0) # 'calibrated_fivepoint.m'가 위치한 경로
+        self.intrinsic = np.array([
+            [1698.873755, 0.000000, 971.7497705],
+            [0.000000, 1698.8796645, 647.7488275],
+            [0.000000, 0.000000, 1.000000]
+        ])
     #Load image
     def load_image(self, img_path, img1_name, img2_name):
         img1 = cv2.imread(img_path + img1_name)
@@ -38,9 +45,9 @@ class SFM():
         res = cv2.drawMatches(img1, img1_kp, img2, img2_kp, sorted_matches, img2, flags=2)
 
         #draw matches
-        plt.figure(figsize=(15,15))
-        plt.imshow(res)
-        plt.show()
+        #plt.figure(figsize=(15,15))
+        #plt.imshow(res)
+        #plt.show()
         return matches_good, img1_kp, img2_kp
 
 
@@ -50,10 +57,32 @@ class SFM():
         random_idx = np.random.randint(p1p2.shape[0], size=5)
         p1p2_ = p1p2[random_idx,:]
 
-        p1s = p1p2_[:,:2]
-        p2s = p1p2_[:,2:]
+        p1s = p1p2_[:,:3]
+        p2s = p1p2_[:,3:]
         return p1s, p2s
 
+
+    def calibrated_fivepoint(self, p1s, p2s):
+        p1s_tp = p1s.T.tolist()
+        p2s_tp = p2s.T.tolist()
+
+        #pdb.set_trace()
+        p1s_tp = matlab.double(p1s_tp) # a = (3,5)
+        p2s_tp = matlab.double(p2s_tp) # b = (3,5)
+
+        E = self.eng.calibrated_fivepoint(p1s_tp, p2s_tp)
+        E = np.asarray(E).T # (?, 9)
+        #pdb.set_trace()
+        return E
+
+    def normalization_pixel(self, p1, p2):
+        p1_normalized = np.linalg.inv(self.intrinsic)@p1.T
+        p2_normalized = np.linalg.inv(self.intrinsic)@p2.T
+
+        p1_normalized = p1_normalized.T
+        p2_normalized = p2_normalized.T
+
+        return p1_normalized, p2_normalized
 
     def Estimation_E(self, matches_good, img1_kp, img2_kp):
         query_idx = [match.queryIdx for match in matches_good] #1370
@@ -61,48 +90,111 @@ class SFM():
 
         p1 = np.float32([img1_kp[ind].pt for ind in query_idx]) # (1370,2)
         p2 = np.float32([img2_kp[ind].pt for ind in train_idx]) # (1370,2)
+
+        ones_column = np.ones((p1.shape[0], 1), dtype=p1.dtype)
+
+        #rescale to homogeneous coordinate (x,y,1)
+        p1 = np.concatenate((p1, ones_column), axis=1)
+        p2 = np.concatenate((p2, ones_column), axis=1)
         
+        #p1, p2 = self.normalization_pixel(p1, p2)
 
-        focal = 1698.8796645
-        fx, fy = 971.7497705, 647.7488275
-
-        """ RANSAC 직접 구현
+        iterations = 1000
+        best_E =  np.random.rand(3,3)
+        best_inlier_cnt = 0
+        threshold = 0.1
+        #threshold = 5e-7
         for i in range(iterations):
+            #pdb.set_trace()
             p1s, p2s = self.randomsample(p1, p2)
-            pdb.set_trace()
-        """
 
-        E, mask = cv2.findEssentialMat(p1, p2, method=cv2.RANSAC, focal=focal, pp=(fx, fy), maxIters = 500, threshold=1)
-        p1 = p1[mask.ravel()==1] #left image inlier
-        p2 = p2[mask.ravel()==1] #right image inlier
+            E_list = self.calibrated_fivepoint(p1s, p2s)
 
-        return E, p1, p2
+            for E in E_list:
+                err_matrix = np.dot(np.dot(p2, E.reshape(3,3)), p1.T)
+                err_list = err_matrix.diagonal()
+                #pdb.set_trace()
+                mask = np.where(np.abs(err_list) < threshold, 1, 0)
+                curr_inlier_cnt = np.sum(mask)
 
+                #pdb.set_trace()
+                if curr_inlier_cnt > best_inlier_cnt:
+                    best_E = E
+                    best_inlier_cnt = curr_inlier_cnt
+                    p1_inlier = p1[mask == 1]
+                    p2_inlier = p2[mask == 1]
 
-    def EM_Decomposition(self, E, p1, p2):
             
-        #return CM
-        pass
 
-    #Rescale to Homogeneous Coordinate
-    def rescale_point(self, pts1, pts2, length):
-        #return a,b
-        pass
+        #pdb.set_trace()
+        return best_E, best_inlier_cnt, p1_inlier, p2_inlier
+            
 
-    #Intrinsic parameter K
-    def initialize_CM(self, CM):
-        #return Rt0, Rt1
-        pass
+
+    def EM_Decomposition(self, E, p1_inlier, p2_inlier):
+        E = E.reshape(3,3)
+        U, S, VT = np.linalg.svd(E, full_matrices=True)
+        W = np.array([
+            [0, -1, 0],
+            [1, 0, 0],
+            [0, 0, 1]
+        ])
+
+        #Camera Matrix candidates (4)
+        camera_matrix = np.array([
+            np.column_stack((U @ W @ VT, U[:,2])),
+            np.column_stack((U @ W @ VT, -U[:,2])),
+            np.column_stack((U @ W.T @ VT, U[:,2])),
+            np.column_stack((U @ W.T @ VT, -U[:,2])),
+        ])
+
+        EM_cnt = [0, 0, 0, 0]
+        EM0 = np.append(np.eye(3), np.zeros((3,1)), axis=1)
+        E_idx = None
+        CM = np.array([[]])
+
+        p1_normalized, p2_normalized = self.normalization_pixel(p1_inlier, p2_inlier)
+
+        #pdb.set_trace()
+        for i in range(4): 
+            EM1 = camera_matrix[i]
+            for k in range(len(p1_normalized)):
+                #pdb.set_trace()
+                A = np.array([
+                    p1_normalized[k][0]*EM0[2] - EM0[0],
+                    p1_normalized[k][1]*EM0[2] - EM0[1],
+                    p2_normalized[k][0]*EM1[2] - EM1[0],
+                    p2_normalized[k][1]*EM1[2] - EM1[1]
+                ])
+
+                U_A, s_A, V_A = np.linalg.svd(A, full_matrices=True)
+                X = V_A[3] / V_A[3,3]
+                if X[2] > 0 and (EM1@X.T)[2] > 0:
+                    EM_cnt[i] += 1
+        
+        E_idx = np.argmax(EM_cnt)
+        EM1 = camera_matrix[E_idx]
+        return EM0, EM1, p1_normalized, p2_normalized
+
+
 
     #Triangulation
-    def LinearTriangulation(self, Rt0, Rt1, p1, p2):
-        #return VT[3,0:3] / VT[3,3]
-        pass
+    def LinearTriangulation(self, EM0, EM1, p1_normalized, p2_normalized):
+        p3d = []
+        for k in range(len(p1_normalized)):
+            A = np.array([
+                p1_normalized[k][0]*EM0[2] - EM0[0],
+                p1_normalized[k][1]*EM0[2] - EM0[1],
+                p2_normalized[k][0]*EM1[2] - EM1[0],
+                p2_normalized[k][1]*EM1[2] - EM1[1]
+            ])
+            U_A, s_A, V_A = np.linalg.svd(A, full_matrices=True)
+            X = V_A[3]/V_A[3, 3]
+            p3d.append(X)
+        
+        p3d = np.array(p3d)
+        return p3d
 
-
-    def make_3dpoint(self, p1, p2):
-        #return p3ds
-        pass
 
     
     def visualize_3d(self, p3ds):
@@ -114,7 +206,7 @@ class SFM():
         Z = np.concatenate((Z, p3ds[2]))
 
         fig = plt.figure(figsize=(15,15))
-        ax = plt.axed(projection='3d')
+        ax = plt.axes(projection='3d')
         ax.scatter3D(X, Y, Z, c='b', marker='o')
         plt.show()
 
